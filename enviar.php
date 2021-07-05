fbbcec5045ede96498b52fa59fc2fa38d153bd50
fede <federicous@gmail.com>
2021-07-05 02:29:58 -0300

    corrección de contacto.html y enviar.php

--- enviar.php
+++ enviar.php
@@ -1,26 +1,25 @@
 <?php
 $inputName = $_POST['inputName'];
 $inputEmail = $_POST['inputEmail'];
-$inputCliente = $_POST['inputCliente'];
 $listaPrecios = $_POST['listaPrecios'];
+$inputCliente = $_POST['inputCliente'];
 
 $header = 'From: ' . $inputEmail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
-​
+
 $mensaje = "Este mensaje fue enviado por " . $inputName . ",\r\n";
 $mensaje .= "Su e-mail es: " . $inputEmail . " \r\n";
+$mensaje .= "Lista de precios: " . $listaPrecios . " \r\n";
 $mensaje .= "Tipo de cliente: " . $inputCliente . " \r\n";
-$mensaje .= "Recibier lista de precios: " . $listaPrecios . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'federicous@gmail.com'; // El mail donde van a llegar los mensajes
+$para = 'federicous@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header('Location:index.html');
-
+header("Location:index.html");
 ?>
